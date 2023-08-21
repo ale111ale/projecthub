c87e23712c363f00966dc61cc0b60ae475fd4bdb
ale111ale <aletechtips@gmail.com>
2023-08-21 19:44:04 +0200

    adding text

--- explorer.py
+++ explorer.py
@@ -2,7 +2,7 @@ print("##################################PythonExplorer#########################
 print("#                                                                        #")
 print("#                   Welcome to PythonExplorer                            #")
 print("#                                                                        #")
-print("#                          This GUI is in beta                           #")
+print("#                      Use this software Responsibly                     #")
 print("#                                                                        #")
 pritn("#          Thanks for using this GUI please report any bug on this GUI   #")
 print("##########################################################################")
@@ -11,3 +11,6 @@ print("########################################LinkWindow#######################
 print("# Please Help me to find a more efficent way to use this without having to type lots of lines of code #")
 print("#######################################################################################################")
 
+print("For saving to the system the library os is gonna get imported")
+import os 
+print("library os imported!")
