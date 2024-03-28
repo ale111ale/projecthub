d80cc32a57d97bc70e6afb6e19aba6a8addffd4a
ale111ale <helloyoulittlehacker@gmail.com>
2024-03-28 19:35:00 +0100

    used beta instead of prerelease (should be in beta branch)

--- explorer.py
+++ explorer.py
@@ -1,6 +1,6 @@
 
 print("===============================================PythonVER===============================================")
-print("#                                     PythonExprorer git_prerelease Build 35                                #")
+print("#                                     PythonExprorer git_beta Build 35                                #")
 print("#######################################################################################################")
 
 print("this project was made with collaboration from rylmovuk")
