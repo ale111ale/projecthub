e84bfe1a886ef6422d2cabb725c96e21b8f54c73
ale111ale <helloyoulittlehacker@gmail.com>
2024-03-31 12:35:07 +0200

    Updated version number

--- explorer.py
+++ explorer.py
@@ -1,6 +1,6 @@
 
 print("===============================================PythonVER===============================================")
-print("#                                     PythonExprorer git_beta Build 38                                #")
+print("#                                     PythonExprorer git_beta Build 42                                #")
 print("#######################################################################################################")
 
 print("this project was made with collaboration from rylmovuk")
